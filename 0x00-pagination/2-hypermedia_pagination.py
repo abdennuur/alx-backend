a4cb900b81a4bc59396aaa78c24423181090a7bd
abdennuur <abdnourkhatib@gmail.com>
2024-07-20 23:25:58 +0000

    2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
-"""The hypermedia pagination sample.
+"""Task 2
 """
+
 import csv
 import math
 from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """To retrieve index range frm a given page and page size.
+    """To retrieve index range from a given page and page size.
     """
-    strt = (page - 1) * page_size
-    nd = strt + page_size
-    return (strt, nd)
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
-    """the Server class to paginate database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """To initializes new Server instance.
-        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """The cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -36,7 +34,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """To retrieve a page of data.
+        """Retrieve a page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
@@ -47,17 +45,16 @@ class Server:
         return data[strt:nd]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """to retrieve information about a page.
+        """Retrieve information about  page.
         """
-        page_data = self.get_page(page, page_size)
+        data = self.get_page(page, page_size)
         strt, nd = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
-        page_info = {
-            'page_size': len(page_data),
+        return {
+            'page_size': len(data),
             'page': page,
-            'data': page_data,
+            'data': data,
             'next_page': page + 1 if nd < len(self.__dataset) else None,
             'prev_page': page - 1 if strt > 0 else None,
-            'total_pages': total_pages,
+            'total_pages': total_pages
         }
-        return page_info
