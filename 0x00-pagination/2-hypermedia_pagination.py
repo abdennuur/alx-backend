62a21a7278b2cbc618112349701f7cb5f19ad743
abdennuur <abdnourkhatib@gmail.com>
2024-07-20 23:29:39 +0000

    2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""Task 2: Hypermedia pagination
+"""Hypermedia pagination sample.
 """
-
 import csv
 import math
 from typing import Dict, List, Tuple
@@ -10,8 +9,9 @@ from typing import Dict, List, Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Retrieves the index range from a given page and page size.
     """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -20,6 +20,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -38,23 +40,24 @@ class Server:
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
+        strt, nd = index_range(page, page_size)
         data = self.dataset()
-        if start > len(data):
+        if strt > len(data):
             return []
-        return data[start:end]
+        return data[strt:nd]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Retrieve information about page.
+        """Retrieves information about a page.
         """
-        dt = self.get_page(page, page_size)
+        page_data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
-        ttl_pages = math.ceil(len(self.__dataset) / page_size)
-        return {
-            'page_size': len(dt),
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
             'page': page,
-            'data': data,
+            'data': page_data,
             'next_page': page + 1 if end < len(self.__dataset) else None,
             'prev_page': page - 1 if start > 0 else None,
-            'total_pages': ttl_pages
+            'total_pages': total_pages,
         }
+        return page_info
